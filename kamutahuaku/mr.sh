07de7dc504c8b6f6a8ea2c7b4f1325a5f24456ae
hansputera <47862061+hansputera@users.noreply.github.com>
2019-09-14 13:12:04 +0700

    Add files via upload

---
+++ kamutahuaku/mr.sh
@@ -0,0 +1,12 @@
+echo "[!] Mengkloning data dan mangganti ke halaman awal [!]"
+cd $HOME
+mkdir mryouknowme
+cd mryouknowme
+git clone https://github.com/xiosveganzy/5tools-.git
+git clone https://github.com/hansputera/bashTermux.git
+clear
+echo "[!] Sukses Mengkloning data [!]"
+sleep 1
+clear
+cd $HOME
+exit
