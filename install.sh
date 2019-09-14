7758b08256a93616d3fea034ecc46114996868bf
hansputera <47862061+hansputera@users.noreply.github.com>
2019-09-14 21:13:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ pkg install ruby
 pip install requests
 pip install mechanize
 pip install bs4
-pip install urlib
+pip install urllib
 pip install beautifulsoup
 pip2 install requests
 pip2 install mechanize
