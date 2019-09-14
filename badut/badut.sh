e22db2ab42e1da9878a524ecacb0e0f92a91de0f
hansputera <47862061+hansputera@users.noreply.github.com>
2019-09-14 13:10:45 +0700

    Add files via upload

---
+++ badut/badut.sh
@@ -0,0 +1,39 @@
+clear
+echo "[!] Mengkloning data serta Mengganti ke halaman termux [!]"
+cd $HOME
+mkdir tuanbadut-tools
+cd tuanbadut-tools
+git clone https://github.com/TUANB4DUT/BACOL
+git clone https://github.com/TUANB4DUT/NIKKK
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+git clone https://github.com/TUANB4DUT/ADLOG
+git clone https://github.com/TUANB4DUT/ARITMATIKA
+git clone https://github.com/TUANB4DUT/PERKIRAAN-CUACA
+git clone https://github.com/TUANB4DUT/SHORTNERLINKS
+git clone https://github.com/TUANB4DUT/FACEBOOK-BRUTEFORCE
+git clone https://github.com/TUANB4DUT/VULNSCANNING
+git clone https://github.com/TUANB4DUT/ENCRYPT-PYTHON
+git clone https://github.com/TUANB4DUT/BASH-ENCRYPT
+git clone https://github.com/TUANB4DUT/TOOLKIT
+git clone https://github.com/TUANB4DUT/JADWALSHOLAT
+git clone https://github.com/TUANB4DUT/YOUTUBE
+git clone https://github.com/TUANB4DUT/HTTPHEADERS
+git clone https://github.com/TUANB4DUT/SUBDO
+git clone https://github.com/TUANB4DUT/DIRHUNT
+git clone https://github.com/TUANB4DUT/WEBDAV
+git clone https://github.com/TUANB4DUT/REVERSEDNS
+git clone https://github.com/TUANB4DUT/WHOIS
+git clone https://github.com/TUANB4DUT/DNSLOOKUP
+git clone https://github.com/TUANB4DUT/TRACKIP
+git clone https://github.com/TUANB4DUT/REVERSEIP
+git clone https://github.com/TUANB4DUT/DIR
+git clone https://github.com/TUANB4DUT/VIRUS
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv2
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+clear
+echo "[!] Sukses mengkloning data [!]"
+echo "[>] Gratis Ges' :v"
+clear
+cd $HOME
+exit
