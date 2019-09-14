eb63868e872c4427a6f06c71d9a9b9db3a1b6d89
hansputera <47862061+hansputera@users.noreply.github.com>
2019-09-14 13:07:12 +0700

    Add files via upload

---
+++ zlucifer/zlucifer.sh
@@ -0,0 +1,29 @@
+echo "[!] Mengkloning Tulz milik zlucifer [!]"
+echo "Memindahkan ke Halaman Termux"
+cd $HOME
+mkdir zlucifer-tools
+cd zlucifer-tools
+git clone https://github.com/zlucifer/paladin_project.git
+git clone https://github.com/zlucifer/troll_project.git
+git clone https://github.com/zlucifer/termux_insta.git
+git clone https://github.com/zlucifer/trap_project.git
+git clone https://github.com/zlucifer/Alice_Al.git
+git clone https://github.com/zlucifer/all_seeing.git
+git clone https://github.com/zlucifer/chaos_angel.git
+git clone https://github.com/zlucifer/skull_project.git
+git clone https://github.com/zlucifer/Raspberry.git
+git clone https://github.com/zlucifer/phoenix_project.git
+git clone https://github.com/zlucifer/light_bringer2.git
+git clone https://github.com/zlucifer/sms_bomb.git
+git clone https://github.com/zlucifer/light_bringer.git
+git clone https://github.com/zlucifer/call.git
+git clone https://github.com/zlucifer/cek_ip.git
+git clone https://github.com/zlucifer/eagle_project.git
+git clone https://github.com/zlucifer/termux_nik.git
+git clone https://github.com/zlucifer/lucita_ddos.git
+git clone https://github.com/zlucifer/nmap_lite.git
+git clone https://github.com/zlucifer/termux_alice.git
+clear
+echo "[>] Sukses Mengkloning data [<]"
+clear
+exit
